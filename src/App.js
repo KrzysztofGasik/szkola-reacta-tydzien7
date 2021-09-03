6ee7f08068f54e7c304968d6a630b27eb3cb09eb
KrzGas <kgasik@gmail.com>
2021-09-03 14:12:42 +0200

    tydzień 7 zadanie 1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Zadanie1 from './components/Zadanie1';
+
+function App() {
+	return (
+		<div className="App">
+			<Zadanie1/>
+		</div>
+	);
+}
+
+export default App;
