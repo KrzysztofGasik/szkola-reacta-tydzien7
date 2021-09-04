18ddf908ae6acbf1c2aa6eb231b98bf930345749
KrzGas <kgasik@gmail.com>
2021-09-04 21:12:31 +0200

    tydzień 7 zadania 1-3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Zadanie1 from './components/Zadanie1';
 
 function App() {
--- src/components/Zadanie1/UserDetails/UserDetails.js
+++ src/components/Zadanie1/UserDetails/UserDetails.js
@@ -1,15 +1,30 @@
 import { NavLink, useParams } from "react-router-dom";
 
 import "./UserDetails.css";
-import User from '../User';
+import User from "../User";
+import { useEffect } from "react/cjs/react.development";
 
-export default function UserDetails({data}) {
+export default function UserDetails({ data }) {
   const userData = useParams();
-  const searchUser = data.slice(0).filter(user=>user.login.uuid === userData.id)[0];
+  const searchUser = data
+    .slice(0)
+    .filter((user) => user.login.uuid === userData.id)[0];
+  useEffect(() => {
+    const latitude = Number(searchUser.location.coordinates.latitude);
+    const longitude = Number(searchUser.location.coordinates.longitude);
+    const L = window.L;
+    const map = L.map("map").setView([latitude,longitude], 5);
+    const marker = L.marker([latitude,longitude]).addTo(map);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+  }, []);
   return (
     <div className="UserDetails">
-        <User data={searchUser} />
-        <NavLink to="/">Go back to all users</NavLink>
+      <User data={searchUser} />
+      <NavLink to="/">Go back to all users</NavLink>
+      <div id="map"></div>
     </div>
   );
 }
