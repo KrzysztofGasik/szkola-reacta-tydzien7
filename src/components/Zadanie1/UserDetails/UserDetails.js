a7522dffbb667b3cdc2fa297305853121d024fd0
KrzGas <kgasik@gmail.com>
2021-09-09 10:15:03 +0200

    refactor

--- src/components/Zadanie1/UserDetails/UserDetails.js
+++ src/components/Zadanie1/UserDetails/UserDetails.js
@@ -2,24 +2,29 @@ import { NavLink, useParams } from "react-router-dom";
 
 import "./UserDetails.css";
 import User from "../User";
-import { useEffect } from "react/cjs/react.development";
+import { useEffect } from "react";
 
 export default function UserDetails({ data }) {
   const userData = useParams();
   const searchUser = data
     .slice(0)
     .filter((user) => user.login.uuid === userData.id)[0];
+  const latitude = Number(searchUser.location.coordinates.latitude);
+  const longitude = Number(searchUser.location.coordinates.longitude);
+
+  console.log(latitude,longitude)
+
   useEffect(() => {
     const latitude = Number(searchUser.location.coordinates.latitude);
     const longitude = Number(searchUser.location.coordinates.longitude);
     const L = window.L;
-    const map = L.map("map").setView([latitude,longitude], 5);
-    const marker = L.marker([latitude,longitude]).addTo(map);
+    const map = L.map("map").setView([latitude, longitude], 5);
+    L.marker([latitude, longitude]).addTo(map);
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
-  }, []);
+  }, [latitude, longitude]);
   return (
     <div className="UserDetails">
       <User data={searchUser} />
--- src/components/Zadanie1/Wrapper.js
+++ src/components/Zadanie1/Wrapper.js
@@ -3,25 +3,13 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UserDetails from "./UserDetails";
 import UserList from "./UserList";
+import getUsers from "./getUsers";
 
 export default function Wrapper() {
   const [users, setUsers] = useState([]);
 
-  async function getUsers() {
-    let res = await fetch("https://randomuser.me/api/?results=10");
-    let data = await res.json();
-    return data;
-  }
-
   useEffect(() => {
-    getUsers()
-      .then((data) => {
-        const apiUsers = Object.values(data)[0];
-        setUsers(apiUsers);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    getUsers(setUsers);
   }, []);
 
   return (
